308ebf5253cb9518e3653c497cb490a75382b5e4
Dheeraj <dheerajagarwal1995@gmail.com>
2026-06-18 14:45:40 +0530

    changes

--- config/indexnow.php
+++ config/indexnow.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'enabled' => env('INDEXNOW_ENABLED', true),
+    'enabled' => env('INDEXNOW_ENABLED', true) && env('APP_ENV') === 'production',
     'key' => env('INDEXNOW_KEY'),
     'host' => parse_url(env('APP_URL', 'http://localhost'), PHP_URL_HOST),
 ];
